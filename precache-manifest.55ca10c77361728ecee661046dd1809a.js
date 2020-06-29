6bfda5e561e814844471c28ae2357fc283ff15b8
Sagar Shrestha <sagar.shrestha@bigmobile.com>
2020-06-29 12:33:12 +1000

    Updates

---
+++ precache-manifest.55ca10c77361728ecee661046dd1809a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf47f7d0352fecdc07bae4162c0b09a2",
+    "url": "/batch-geocoding-using-react/index.html"
+  },
+  {
+    "revision": "932b2475a11a8142e3d8",
+    "url": "/batch-geocoding-using-react/static/css/2.8f3cb0f3.chunk.css"
+  },
+  {
+    "revision": "932b2475a11a8142e3d8",
+    "url": "/batch-geocoding-using-react/static/js/2.7a1b8180.chunk.js"
+  },
+  {
+    "revision": "d58513edbdaca00e817e",
+    "url": "/batch-geocoding-using-react/static/js/main.e1330ebc.chunk.js"
+  },
+  {
+    "revision": "2cf628e196d54e06f571",
+    "url": "/batch-geocoding-using-react/static/js/runtime~main.59d26c9b.js"
+  }
+]);
