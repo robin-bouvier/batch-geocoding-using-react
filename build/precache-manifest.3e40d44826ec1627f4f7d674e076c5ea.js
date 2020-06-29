3bb41c0c26e77fced7dc1b665fe27421acb8701c
Sagar Shrestha <sagar.shrestha@bigmobile.com>
2020-06-29 12:30:57 +1000

    built to deploy in github

---
+++ build/precache-manifest.3e40d44826ec1627f4f7d674e076c5ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fddc98dbc7984b6ef655f2593c8eb23c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "932b2475a11a8142e3d8",
+    "url": "/static/css/2.8f3cb0f3.chunk.css"
+  },
+  {
+    "revision": "932b2475a11a8142e3d8",
+    "url": "/static/js/2.7a1b8180.chunk.js"
+  },
+  {
+    "revision": "d58513edbdaca00e817e",
+    "url": "/static/js/main.e1330ebc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
